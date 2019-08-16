721b3bcbb01788f09a173e15b152d137ea60cb23
saurssauravjs <saurav.maharjan@fusemachines.com>
2019-08-17 00:19:36 +0545

    chore(demo): demo page updated

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Pagination from "../lib/Pagination";
-// import "./app.css";
+import "./app.css";
 
 // const App = () => (
 
@@ -13,63 +13,199 @@ class App extends React.Component {
     };
     render() {
         return (
-            <div>
-                <div className="container">
-                    <h1>React Pagination</h1>
-                    <p>
-                        current Page: <strong>{this.state.currentPage}</strong>
-                    </p>
-                    <hr />
-                    <h3>Default</h3>
 
-                    <Pagination
-                        currentPage={this.state.currentPage}
-                        totalPages={10}
-                        changeCurrentPage={this.changeCurrentPage}
-                        theme="default"
-                        showFirstLastPages={true}
-                    />
-                    <h3>Dark</h3>
-                    <Pagination
-                        currentPage={this.state.currentPage}
-                        totalPages={10}
-                        changeCurrentPage={this.changeCurrentPage}
-                        theme="dark"
-                        showFirstLastPages={true}
-                    />
+            <div className="container">
+                <a class="github-fork-ribbon" href="https://github.com/isaurssaurav/react-pagination-js" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
+                <h1>React Pagination Js</h1>
+                <h2>Simple and light weight</h2>
+                {/* <p>
+                    current Page: <strong>{this.state.currentPage}</strong>
+                </p> */}
+                <hr />
+                <div className="box-theme">
 
+                    <p>Theme : "<strong>bootstrap</strong>"</p>
+                    <div className="demo">
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+                            theme="bootstrap"
+                        />
 
-                    <h3>Border Bottom</h3>
+                    </div>
+                    <div className="code">
+                        <code >{` <Pagination\n
+                            currentPage={this.state.currentPage}\n
+                            totalPages={10}\n
+                            changeCurrentPage={this.changeCurrentPage}\n
+                            theme="bootstrap"\n
+                        />`}
 
-                    <Pagination
-                        currentPage={this.state.currentPage}
-                        totalPages={10}
-                        changeCurrentPage={this.changeCurrentPage}
-                        theme="border-bottom"
-                    />
-
-                    <h3>Bootstrap</h3>
-
-                    <Pagination
-                        currentPage={this.state.currentPage}
-                        totalPages={10}
-                        changeCurrentPage={this.changeCurrentPage}
-                        theme="bootstrap"
-                    />
-
-
-                    <h3>Circle</h3>
-
-                    <Pagination
-                        currentPage={this.state.currentPage}
-                        totalPages={10}
-                        changeCurrentPage={this.changeCurrentPage}
-                        theme="circle"
-                    />
-
+                        </code>
+                    </div>
                 </div>
+                <div className="box-theme">
+
+                    <p>Theme : "<strong>default</strong>"</p>
+                    <div className="demo">
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+                            theme="default"
+                            showFirstLastPages={true}
+                        />
+                    </div>
+                    <div className="code">
+                        <code >{`<Pagination \n
+                            currentPage={this.state.currentPage}\n
+                            totalPages={10}\n
+                            changeCurrentPage={this.changeCurrentPage}\n
+                    />`}
+
+                        </code>
+                    </div>
+                </div>
+                <div className="box-theme">
+
+                    <p>Theme : "<strong>dark</strong>"</p>
+                    <div className="demo">
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+                            theme="dark"
+                            showFirstLastPages={true}
+                        />
+                    </div>
+                    <div className="code">
+                        <code >{`<Pagination\n
+                    currentPage={this.state.currentPage}\n
+                    totalPages={10}\n
+                    changeCurrentPage={this.changeCurrentPage}\n
+                    theme="dark"\n
+                />`}
+
+                        </code>
+                    </div>
+                </div>
+
+
+                <div className="box-theme">
+
+                    <p>Theme : "<strong>border-bottom</strong>"</p>
+                    <div className="demo">
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+                            theme="border-bottom"
+                        />
+                    </div>
+                    <div className="code">
+                        <code >{`<Pagination\n
+                            currentPage={this.state.currentPage}\n
+                            totalPages={10}\n
+                            changeCurrentPage={this.changeCurrentPage}\n
+                            theme="border-bottom"\n
+                        />`}
+
+                        </code>
+                    </div>
+                </div>
+
+
+                <div className="box-theme">
+
+                    <p>Theme : "<strong>circle</strong>"</p>
+                    <div className="demo">
+
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+                            theme="circle"
+                        />
+
+                    </div>
+                    <div className="code">
+                        <code >{` 
+                            <Pagination\n
+                                currentPage={this.state.currentPage}\n
+                                totalPages={10}\n
+                                changeCurrentPage={this.changeCurrentPage}\n
+                                theme="circle"\n    
+                            />`}
+
+                        </code>
+                    </div>
+                </div>
+
+                <div className="box-theme">
+
+                    <p> <strong>Hide first and last page</strong></p>
+                    <div className="demo">
+
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+
+                            showFirstLastPages={false}
+                        />
+
+                    </div>
+                    <div className="code">
+                        <code >{` 
+                                <Pagination\n 
+                                currentPage={this.state.currentPage}\n 
+                                totalPages={10}\n 
+                                changeCurrentPage={this.changeCurrentPage}\n 
+                                showFirstLastPages={false}\n 
+                                />`}
+
+                        </code>
+                    </div>
+                </div>
+
+                <div className="box-theme">
+
+                    <p> <strong>Change text of first, last, next and previous page text</strong></p>
+                    <div className="demo">
+
+                        <Pagination
+                            currentPage={this.state.currentPage}
+                            totalPages={10}
+                            changeCurrentPage={this.changeCurrentPage}
+                            firstPageText="first"
+                            lastPageText="last"
+                            showFirstLastPages={true}
+                            nextPageText="next"
+                            previousPageText="prev"
+                        />
+
+                    </div>
+                    <div className="code">
+                        <code >{`  <Pagination \n
+                            currentPage={this.state.currentPage}\n
+                            totalPages={10}\n
+                            changeCurrentPage={this.changeCurrentPage}\n
+                            firstPageText="first"\n
+                            lastPageText="last"\n
+                            showFirstLastPages={true}\n
+                            nextPageText="next"\n
+                            previousPageText="prev"\n
+                        />
+`}
+
+                        </code>
+                    </div>
+                </div>
+
             </div>
         );
     }
 }
 export default App;
+
